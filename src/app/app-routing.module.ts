f9ea5e1bef04686683ac494b3f8a5aa52b4a2558
Waheed <wahid.khan74@gmail.com>
2022-05-28 17:07:17 +0000

    added auth moule with login , register and change password screens

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ChangePasswordComponent } from './components/auth/change-password/change-password.component';
+import { LoginComponent } from './components/auth/login/login.component';
+import { RegisterComponent } from './components/auth/register/register.component';
+import { HomeComponent } from './components/home/home.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
+import { ProductsComponent } from './components/products/products.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path:"" , redirectTo:"/home", pathMatch:"full"},
+  { path:"home" , component:HomeComponent},
+  { path:"register" , component:RegisterComponent},
+  { path:"login" , component:LoginComponent},
+  { path:"change-password" , component:ChangePasswordComponent},
+  { path:"products" , children: [
+    {path :"", component:ProductsComponent},
+  ]},
+  { path:"**" , component:NotFoundComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
